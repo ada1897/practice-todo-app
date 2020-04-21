d35fc3dc248132206d6e433b95123b0f1753a665
vagrant <vagrant@homestead>
2020-04-21 12:23:12 +0000

    7終わり

--- config/database.php
+++ config/database.php
@@ -35,6 +35,13 @@ return [
 
     'connections' => [
 
+        // テスト実行用のデータベース
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ],
+
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
